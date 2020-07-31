8eaa067ea4afdfeb11d7069ab64de2c7c29dd559
Duwan Peña <duwanp2112@gmail.com>
2020-07-30 19:38:07 -0430

    Updates

---
+++ precache-manifest.65933fd4934d57ef148c87d7bfb76435.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "811705835fcc9a3fc8e750615e79c331",
+    "url": "/CotizarCripto/index.html"
+  },
+  {
+    "revision": "fae19634b8d37728934e",
+    "url": "/CotizarCripto/static/css/main.97cd5f8b.chunk.css"
+  },
+  {
+    "revision": "0dd21a11164075ea27be",
+    "url": "/CotizarCripto/static/js/2.b2e0ea8f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CotizarCripto/static/js/2.b2e0ea8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fae19634b8d37728934e",
+    "url": "/CotizarCripto/static/js/main.647a311b.chunk.js"
+  },
+  {
+    "revision": "35440204c9c74a3c01c8",
+    "url": "/CotizarCripto/static/js/runtime-main.0ccea8a3.js"
+  },
+  {
+    "revision": "96e45d694833f161c9dc02ab9dde4a6b",
+    "url": "/CotizarCripto/static/media/cryptomonedas.96e45d69.png"
+  }
+]);
